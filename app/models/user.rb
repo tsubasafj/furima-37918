4972af3de15ab0aa0075c2df02a0498a07a85611
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-07-28 00:03:37 +0900

    新規登録画面の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+
+  has_many :items
+  has_many :purchases
 end
