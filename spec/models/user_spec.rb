ce72769bf1d0987d9a0e47c3b989ad72a884b116
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-01 16:33:15 +0900

    gem追加導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,14 +7,16 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it 'nickname,email,password,password_confirmation,last_name,first_name,
-      last_name_kana,first_name_kana,birth_dateが存在すれば登録できる' do
-        
+      it 'nickname,email,password,password_confirmation,last_name,first_name,last_name_kana,first_name_kana,birth_dateが存在すれば登録できる' do
+        expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
       it 'nicknameが空では登録できない' do
-        
+        @user.nickname = ""
+        @user.valid?
+        binding.pry
+        expect(@user.errors.full_messages).to include()
       end
       it 'emailが空では登録できない' do
         
