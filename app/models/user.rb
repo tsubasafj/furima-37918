377bfa7594f14b1e123b470d6b039b1a295b0615
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-01 17:39:03 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,14 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
   
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name, :first_name, format: { with: VALID_NAME_REGEX }
+
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
 
   has_many :items
   has_many :purchases
