79e763ebe839658c37613efae6a0792f1c22796c
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-01 17:39:57 +0900

    FactoryBot記述変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
     last_name { person.last.kanji }
     first_name { person.first.kanji }
