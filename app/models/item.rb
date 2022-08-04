f9f27eaf33216c9ce895b7949239aab15edabc09
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-04 18:39:00 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one :purchase
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_time
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_time_id, numericality: { other_than: 1 }
+
+  VALID_PRICE_REGEX = /\A[0-9]+\z/
+  validates :price, format: { with: VALID_PASSWORD_REGEX }, numericality: { in: 300..9999999 }
 end
