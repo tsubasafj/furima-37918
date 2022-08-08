969a4ab69d671f492ffa741b0c9ead6ea5ea46f0
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-08 19:15:56 +0900

    テストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '商品の出品ができる場合' do
+      it 'image,name,content,price,category,condition,shipping_fee,prefecture,shipping_timeが存在していれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品の出品ができない場合' do
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it 'nameが空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it 'nameが40文字以上では出品できない' do
+        @item.name = Faker::Lorem.characters(number:41)
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name is too long (maximum is 40 characters)"
+      end
+      it 'contentが空では出品できない' do
+        @item.content = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Content can't be blank"
+      end
+      it 'contentが1000文字以上では出品できない' do
+        @item.content = Faker::Lorem.characters(number:1001)
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Content is too long (maximum is 1000 characters)"
+      end
+      it 'categoryが空では出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category is not a number"
+      end
+      it 'conditionが空では出品できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition is not a number"
+      end
+      it 'shipping_feeが空では出品できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee is not a number"
+      end
+      it 'prefectureが空では出品できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture is not a number"
+      end
+      it 'shipping_timeが空では出品できない' do
+        @item.shipping_time_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping time is not a number"
+      end
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+      it 'priceが全角数字では出品できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+    end
+  end
 end
