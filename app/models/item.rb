9396d3305d08163e199fb48528e80c825ab45b65
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-09 20:24:27 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,8 +18,5 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_time_id, numericality: { other_than: 1 }
-
-  VALID_PRICE_REGEX = /\A[0-9]+\z/
-  validates :price, presence: true, format: { with: VALID_PRICE_REGEX },
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
