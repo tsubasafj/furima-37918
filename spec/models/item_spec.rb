03c5043966a0e3c9db6b3ac47bb7580de2d40c12
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-09 20:24:49 +0900

    テストコード追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -82,6 +82,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
+      it 'userが紐づいていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
+      end
     end
   end
 end
