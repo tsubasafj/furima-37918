65e7726db8d744978ea1e7c8fe73998283501feb
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-23 22:21:23 +0900

    Formオブジェクトの作成

---
+++ app/models/purchase_shipping_address.rb
@@ -0,0 +1,26 @@
+class PurchaseShippingAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :zip_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number, :purchase_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+
+    VALID_ZIPCODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+    validates :zip_code, format: { with: VALID_ZIPCODE_REGEX }
+
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address_line1
+
+    VALID_PHONENUMBER_REGEX = /\A[0-9]{10,11}\z/
+    validates :phone_number, format: { with: VALID_PHONENUMBER_REGEX} 
+    
+    validates :purchase_id
+  end
+
+  def save
+    purchase = Purchase.crete(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, address_line1: address_line1, address_line2: address_line2, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
