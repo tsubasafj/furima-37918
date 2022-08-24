02a26ac1b85f674d02bb3c777d14bf27f032bebe
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-24 21:00:11 +0900

    purchaseコントローラーの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+
+  def create
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping_address).permit(:zip_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
