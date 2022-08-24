6cecaeb59826d5892c4a6337fd68a949fc3b98fe
tsubasafj <105718145+tsubasafj@users.noreply.github.com>
2022-08-24 21:06:07 +0900

    Formオブジェクト修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -15,12 +15,10 @@ class PurchaseShippingAddress
 
     VALID_PHONENUMBER_REGEX = /\A[0-9]{10,11}\z/
     validates :phone_number, format: { with: VALID_PHONENUMBER_REGEX} 
-    
-    validates :purchase_id
   end
 
   def save
-    purchase = Purchase.crete(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     ShippingAddress.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, address_line1: address_line1, address_line2: address_line2, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
